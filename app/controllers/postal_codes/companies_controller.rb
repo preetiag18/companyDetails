d1b0d87ada5e359a8160d85b9882223d27d18d91
preetiag18 <preetiag18@gmail.com>
2023-03-29 23:30:39 +0300

    exclude date fields from the getCompanies response

--- app/controllers/postal_codes/companies_controller.rb
+++ app/controllers/postal_codes/companies_controller.rb
@@ -2,6 +2,6 @@ class PostalCodes::CompaniesController < ApplicationController
     # GET /:postcode/companies
     def show
         @company = Company.where(:postcode => params[:postcode])
-        render json: @company
+        render json: @company, :except => [:created_at, :updated_at]
     end
 end
